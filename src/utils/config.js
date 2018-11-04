c0cc572cf2357e7fc915ccba6b28d51335b949c0
Gerivan Santos <gerivantpc3@gmail.com>
2018-11-04 16:22:45 -0200

    connection heroku postgres dp

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,9 +4,17 @@ const config = {
         username: process.env.CS_DB_USER || 'lvmlzjewscwulv',
         password: process.env.CS_DB_PASSWORD || 'b5a13f1bd7f6f213812e30e8fe73d8392188ecb1688daa05c203b4db0c5bce5d',
         database: process.env.CS_DB || 'dd6qrf175nihue', //'desafioConcrete',
-        port: 5432,
-        ssl: true
-      },
+        port: 5432,    
+        native: true,  
+    },
+
+    // db: {
+    //     host: 'localhost',
+    //     username: process.env.CS_DB_USER || 'postgres',
+    //     password: process.env.CS_DB_PASSWORD || 'avantia123',
+    //     database: process.env.CS_DB || 'desafioConcrete',
+    //     port: 5432    
+    // },
 
     message: {
 
--- src/utils/sequelize_config.js
+++ src/utils/sequelize_config.js
@@ -2,15 +2,23 @@ var Sequelize = require('sequelize'),
     path = require('path'),
     config = require(path.resolve('src/utils/config'));
 
-var sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
-  host: config.db.host,
-  dialect: 'postgres',
-  logging: false,
-  pool: {
-    max: 20,
-    min: 5,
-    idle: 10000
-  }
+//const sequelize = new Sequelize('postgres://lvmlzjewscwulv:b5a13f1bd7f6f213812e30e8fe73d8392188ecb1688daa05c203b4db0c5bce5d@ec2-184-73-169-151.compute-1.amazonaws.com:5432/dd6qrf175nihue sslmode=require');
+
+ var sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
+   host: config.db.host,
+   dialect: 'postgres',
+   ssl:true,
+   dialectOptions:{
+      ssl:{
+         require:true
+      }
+   },
+   logging: false, 
+   pool: {
+     max: 20,
+     min: 5,
+     idle: 10000,
+   }
 });
 
 module.exports = sequelize;
